4af3064be5e0291334d9adb53673309aee74360c
juandsoto <juandsoto1006@gmail.com>
2023-02-02 19:51:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starter code
+
+run `yarn` and `yarn dev` to run the app.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <h1 className="text-2xl">New Project</h1>
+    </div>
+  );
+}
+
+export default App;
